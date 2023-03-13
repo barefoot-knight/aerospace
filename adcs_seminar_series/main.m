22a12c7b1f1a245914c6a67223c106ee36328d9e
barefoot-knight <52506335+barefoot-knight@users.noreply.github.com>
2023-03-12 21:09:32 -0500

    Fix IGRF path in main

--- adcs_seminar_series/main.m
+++ adcs_seminar_series/main.m
@@ -23,7 +23,7 @@ disp('Simulation Started')
 disp(['You must download the igrf model from MathWorks in order to' ...
       ' use this software'])
 disp('https://www.mathworks.com/matlabcentral/fileexchange/34388-international-geomagnetic-reference-field-igrf-model')
-addpath '../igrf/'
+addpath './igrf/'
 nextMagUpdate = 1;
 lastMagUpdate = 0;
 
